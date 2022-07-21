fc13eaec414950393e138bae4e84a7b6ae5bbde5
BENARD Patrick <pbenard73@gmail.com>
2022-07-21 19:45:53 +0200

    wip

--- App.js
+++ App.js
@@ -42,15 +42,39 @@ const App = () => {
 
         return (<p onClick={() => navigate(`/${item.path}`)}>{item.name}</p>)
     })
-
     return (
         <div class="container-fluid">
             { location.pathname === '/' && (
+                <>
                 <div className="intro" style={{height:'100vh'}} >
+           
                     <div style={{backgroundImage:`url(${utils.assets('bg.jpg', true)}`}}></div>
                     <h1>{templateData.title || 'Floral Theme'}</h1>
-                    
+                    <h2>{templateData.subtitle || 'A wonderful floral theme'}</h2>
+                    <div className='godown'>
+                        <a onClick={() => {
+                            document.body.scrollTo(0, document.querySelector('.menu + *').offsetTop)
+                        }}>
+                            <img src={utils.assets('flower_arrow.svg', true)} />
+                        </a>
+                    </div>
                 </div>
+                <nav className="menu">
+                        {templateData.home_menu.map(item => {
+                            let param = {}
+                            if (item[1].slice(0,1) === '/') {
+                                param.onClick=() => navigate(item[1])
+                            } else if (item[1].slice(0,1) === '#') {
+                                param.onClick=() => document.body.scrollTo(0, document.querySelector(item[1]).offsetTop)
+                            } else {
+                                param.href=item[1]
+                            }
+                            return (
+                                <a {...param}>{item[0]}</a>
+                            )
+                        })}
+                    </nav>
+                </>
             )}
                         <Routes>
                         {pageData.map(page => (
