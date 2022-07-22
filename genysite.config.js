30bb3737092f494b6573d86822e069f9f4f0dc18
BENARD Patrick <pbenard73@gmail.com>
2022-07-22 19:49:43 +0200

    next dev

---
+++ genysite.config.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+module.exports = {
+  homepage: "file:///home/patrick/Workspace/themes/genysite-floral/docs/",
+  react:true,
+  index:'index',
+  App: `${__dirname}/src/template/App.js`,
+  index_html: `${__dirname}/src/template/index.html`,
+  data: {
+    home_menu: [
+      ['Css Styles & Components', '/css'],
+      ['Configuration Variables', '/configuration']
+    ],
+    title: 'Genysite Floral Theme',
+    menu: [
+      ['Home', '/'],
+      ['Css Styles & Components', '/css'],
+      ['Configuration & Variables', '/configuration']
+    ],
+    metadata: [
+      '<meta tamere="snif" />',
+      () =>  '<meta tonpere="leplombier" />'
+    ],
+    copyright: '@2022 Genysite Theme'
+  },
+  postScript: function(compile) {
+    this.data.footer = compile(`${__dirname}/src/footer.njk`)
+
+    return this;
+  }
+}
